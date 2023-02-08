e0b2a460973f163f832577dcf37ed0ad186a64ed
Anurag <anuragf45@gmail.com>
2023-02-08 16:43:50 +0530

    methods

---
+++ methods.java
@@ -0,0 +1,16 @@
+public class methods {
+
+
+        public static void evenOdd(int n){
+            if(n%2==0){
+                System.out.println(n+" is Even");
+            }else{
+                System.out.println(n+" is Odd");
+            }
+        }
+        public static void main(String args[]){
+            evenOdd(45);
+            evenOdd(44);
+        }
+    }
+

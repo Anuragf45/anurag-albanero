ca2150554ef61a27e2cae954ff953c4ccb47ff76
Anurag <anuragf45@gmail.com>
2023-02-09 12:34:38 +0530

    fibonacci

--- factorial.java
+++ factorial.java
@@ -1,11 +1,14 @@
 public class factorial {
     public static void main(String[] args) {
-        int n=5;
-        int a=1;
-        while(n>0){
-            a=a*n;
-            n=n-1;
-        }
-        System.out.println(a);
+      int n1=0,n2=1,n3;
+      int n=5;
+        System.out.print(n1+" "+n2);
+      for(int i=2;i<=n;i++){
+          n3=n1+n2;
+          System.out.print(" "+n3);
+          n1=n2;
+          n2=n3;
+      }
+
     }
 }

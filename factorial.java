244b24aeee943e22ebd71601b3c03669e832864b
Anurag <anuragf45@gmail.com>
2023-02-09 12:21:35 +0530

    factorial

---
+++ factorial.java
@@ -0,0 +1,11 @@
+public class factorial {
+    public static void main(String[] args) {
+        int n=5;
+        int a=1;
+        while(n>0){
+            a=a*n;
+            n=n-1;
+        }
+        System.out.println(a);
+    }
+}
---
+++ fibinacci.java
@@ -0,0 +1,5 @@
+public class fibinacci {
+    public static void main(String[] args) {
+
+    }
+}
